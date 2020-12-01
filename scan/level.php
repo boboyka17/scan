0caf33f69393a7755c8b5971449110faf7820b17
boboyka17 <boboyka15@gmail.com>
2020-12-01 23:54:00 +0700

    clear useless file

--- scan/level.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_POST)) {
-    include('connect.php');
-    $id = $_POST["id"];
-    $l = $_POST["level"];
-    $sql = sprintf("UPDATE `scan2557` SET type123='%s' WHERE std_id = '%s'", $l, $id);
-    if (!$conn->query($sql)) die($conn->error);
-}
